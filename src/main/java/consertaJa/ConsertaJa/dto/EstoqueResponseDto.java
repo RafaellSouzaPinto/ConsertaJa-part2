ae920980ab46078e95a0b4b007fe6cb7d523fc3c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-02 20:30:30 -0300

    Primeiro commit - adicionando projeto ConsertaJa

---
+++ src/main/java/consertaJa/ConsertaJa/dto/EstoqueResponseDto.java
@@ -0,0 +1,27 @@
+package consertaJa.ConsertaJa.dto;
+
+import consertaJa.ConsertaJa.model.Estoque;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public record EstoqueResponseDto(
+        Long id,
+        int capacidade,
+        String localArmazenamento,
+        List<FerramentaResponseDto> ferramentas
+) {
+    public EstoqueResponseDto(Estoque estoque) {
+        this(
+                estoque.getId(),
+                estoque.getCapacidade(),
+                estoque.getLocalArmazenamento(),
+                Optional.ofNullable(estoque.getFerramentas())
+                        .orElseGet(List::of)
+                        .stream()
+                        .map(FerramentaResponseDto::new)
+                        .collect(Collectors.toList())
+        );
+    }
+}
---
+++ src/main/java/consertaJa/ConsertaJa/dto/FerramentaResponseDto.java
@@ -0,0 +1,31 @@
+package consertaJa.ConsertaJa.dto;
+
+import consertaJa.ConsertaJa.model.Classificacao;
+import consertaJa.ConsertaJa.model.Ferramenta;
+import consertaJa.ConsertaJa.model.Tipo;
+
+public record FerramentaResponseDto(
+        Long id,
+        String nome,
+        Tipo tipo,
+        Classificacao classificacao,
+        String tamanho,
+        double preco,
+        int quantidade,
+        Long fornecedorId,
+        Long estoqueId
+) {
+    public FerramentaResponseDto(Ferramenta f) {
+        this(
+                f.getId(),
+                f.getNome(),
+                f.getTipo(),
+                f.getClassificacao(),
+                f.getTamanho(),
+                f.getPreco(),
+                f.getQuantidade(),
+                f.getFornecedor() != null ? f.getFornecedor().getId() : null,
+                f.getEstoque() != null ? f.getEstoque().getId() : null
+        );
+    }
+}
